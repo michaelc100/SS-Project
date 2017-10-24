84e5ae0c0ebb124029de8243ccb4b8a349e62d82
Michael Connolly <miconnol@tcd.ie>
2017-10-24 18:55:19 +0100

    Added code for 2nd (4,4) type

--- SpinModels/metropolis.cpp
+++ SpinModels/metropolis.cpp
@@ -42,18 +42,43 @@ int main()
 	graph1.addEdge(e3);
 	graph1.addEdge(e4);
 
-	double stateCount[5] = {0.0}; //to hold counts of each time system is in a given macrostate
+	double stateCountg1[5] = {0.0}; //to hold counts of each time system is in a given macrostate
 	int runtime = 2000000;
 	double beta = 1.0;
 
 	for (int i = 0; i < runtime; i++)
 	{
 		graph1.update(beta);
-		stateCount[getMacroState(graph1.spins)] += 1.0;
+		stateCountg1[getMacroState(graph1.spins)] += 1.0;
 	}		
 
+	std::cout << "Square type graph\n";
 	for (int i = 0; i < 5; i++)
-		std::cout << "MacroState: " << i << " Prob: " << stateCount[i]/runtime << "\n";
+		std::cout << "MacroState: " << i << " Prob: " << stateCountg1[i]/runtime << "\n";
+
+	SpinGraph<Ising> graph2 = SpinGraph<Ising>(4);
+	
+	//adding edges to make graph weird type
+	Edge f1(1,2);
+	Edge f2(2,3);
+	Edge f3(2,4);
+	Edge f4(3,4);
+	graph2.addEdge(f1);
+	graph2.addEdge(f2);
+	graph2.addEdge(f3);
+	graph2.addEdge(f4);
+
+	double stateCountg2[5] = {0.0};	
+	for (int i = 0; i < runtime; i++)
+	{
+		graph2.update(beta);
+		stateCountg2[getMacroState(graph2.spins)] += 1.0;
+	}		
+
+	std::cout << "Weird type graph\n";
+	for (int i = 0; i < 5; i++)
+		std::cout << "MacroState: " << i << " Prob: " << stateCountg2[i]/runtime << "\n";
+
 
 	return 0;
 }
